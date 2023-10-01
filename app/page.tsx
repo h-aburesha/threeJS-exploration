d81f3333c13c5e09688405363435c52a32bee13c
HASSAN <hassan.aburesha@gmail.com>
2023-10-01 17:22:13 +0200

    loaded 3d model

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Shiba } from "./components/shiba";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main style={{ width: "100vw", height: "100vh" }}>
             <Shiba />
         </main>
     );
