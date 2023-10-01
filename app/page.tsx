fafee52d7b56d3dfe2f1b0688d18d9e54177d331
HASSAN <hassan.aburesha@gmail.com>
2023-10-01 17:13:47 +0200

    build fixes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Shiba } from "./components/shiba";
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <Shiba /> // rename it to your component here
+            <Shiba />
         </main>
     );
 }
