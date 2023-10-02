77a0e9727c837b0302493b422f6cb69edad74712
HASSAN <hassan.aburesha@gmail.com>
2023-10-02 18:32:31 +0200

    styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
-import Image from "next/image";
 import { Shiba } from "./components/shiba";
+import { Box } from "@mui/material";
 
 export default function Home() {
     return (
-        <main style={{ width: "100vw", height: "100vh" }}>
-            <Shiba />
+        <main>
+            <div
+                style={{
+                    display: "flex",
+                    maxHeight: 500,
+                    maxWidth: "100%",
+                    padding: 10,
+                    flexDirection: "column",
+                    justifyContent: "center",
+                }}
+            >
+                <Shiba />
+            </div>
         </main>
     );
 }
